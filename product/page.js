d6f05f2590ca2d5013418b2a2ef2822ec8c1b74b
inamulhaqmalik <inamulhaqmalik7575@gmail.com>
2025-10-08 23:10:17 +0500

    changings

---
+++ product/page.js
@@ -0,0 +1,11 @@
+// mart.js
+
+function toggleSearch() {
+    const searchBox = document.getElementById("searchBox");
+    searchBox.style.display = searchBox.style.display === "block" ? "none" : "block";
+}
+
+function toggleUser() {
+    const userBox = document.getElementById("userBox");
+    userBox.style.display = userBox.style.display === "block" ? "none" : "block";
+}
